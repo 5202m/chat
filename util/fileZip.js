07973f80a02ce1a8f664173e8301cf711f8070d0
dick <dick@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-03-14 05:51:48 +0000

    直播间手机版
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@2022 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- util/fileZip.js
+++ util/fileZip.js
@@ -53,24 +53,31 @@ var fileZip = {
 };
 
 //微解盘前端js/css压缩
-fileZip.zipJs(["public\\js\\wechat\\admin.js"],'public\\js\\wechat\\admin.min.js');
-fileZip.zipJs(["public\\js\\wechat\\index.js"],'public\\js\\wechat\\index.min.js');
-fileZip.zipJs(["public\\js\\wechat\\room.js"],'public\\js\\wechat\\room.min.js');
-fileZip.zipCss(["public\\css\\wechat\\admin.css"],'public\\css\\wechat\\admin.min.css');
-fileZip.zipCss(["public\\css\\wechat\\index.css"],'public\\css\\wechat\\index.min.css');
+//fileZip.zipJs(["public\\js\\wechat\\admin.js"],'public\\js\\wechat\\admin.min.js');
+//fileZip.zipJs(["public\\js\\wechat\\index.js"],'public\\js\\wechat\\index.min.js');
+//fileZip.zipJs(["public\\js\\wechat\\room.js"],'public\\js\\wechat\\room.min.js');
+//fileZip.zipCss(["public\\css\\wechat\\admin.css"],'public\\css\\wechat\\admin.min.css');
+//fileZip.zipCss(["public\\css\\wechat\\index.css"],'public\\css\\wechat\\index.min.css');
 
-fileZip.zipJs(["public\\js\\fxchat\\index.js"],'public\\js\\fxchat\\index.min.js');
-fileZip.zipJs(["public\\js\\fxchat\\room.js"],'public\\js\\fxchat\\room.min.js');
-fileZip.zipCss(["public\\css\\fxchat\\index.css"],'public\\css\\fxchat\\index.min.css');
+//fileZip.zipJs(["public\\js\\fxchat\\index.js"],'public\\js\\fxchat\\index.min.js');
+//fileZip.zipJs(["public\\js\\fxchat\\room.js"],'public\\js\\fxchat\\room.min.js');
+//fileZip.zipCss(["public\\css\\fxchat\\index.css"],'public\\css\\fxchat\\index.min.css');
 
 //直播间前端js/css压缩
-fileZip.zipJs(["public\\js\\studio\\admin.js"],'public\\js\\studio\\admin.min.js');
-fileZip.zipJs(["public\\js\\studio\\index.js"],'public\\js\\studio\\index.min.js');
-fileZip.zipCss(["public\\css\\studio\\admin.css"],'public\\css\\studio\\admin.min.css');
-fileZip.zipCss(["public\\css\\studio\\index.css"],'public\\css\\studio\\index.min.css');
+//fileZip.zipJs(["public\\js\\studio\\admin.js"],'public\\js\\studio\\admin.min.js');
+//fileZip.zipJs(["public\\js\\studio\\index.js"],'public\\js\\studio\\index.min.js');
+//fileZip.zipCss(["public\\css\\studio\\admin.css"],'public\\css\\studio\\admin.min.css');
+//fileZip.zipCss(["public\\css\\studio\\index.css"],'public\\css\\studio\\index.min.css');
+
+//直播间前端（手机版）js/css压缩
+//fileZip.zipJs(["public\\js\\studio_mb\\index.js"],'public\\js\\studio_mb\\index.min.js');
+//fileZip.zipJs(["public\\js\\studio_mb\\room.js"],'public\\js\\studio_mb\\room.min.js');
+//fileZip.zipJs(["public\\js\\studio_mb\\pop.js"],'public\\js\\studio_mb\\pop.min.js');
+//fileZip.zipCss(["public\\css\\studio_mb\\index.css"],'public\\css\\studio_mb\\index.min.css');
 
 //通用js/css压缩
-fileZip.zipJs(["public\\js\\util\\common.js"],'public\\js\\util\\common.min.js');
-fileZip.zipJs(["public\\js\\lib\\jQueryRotate.2.2.js"],'public\\js\\lib\\jQueryRotate.min.js');
+//fileZip.zipJs(["public\\js\\util\\common.js"],'public\\js\\util\\common.min.js');
+//fileZip.zipJs(["public\\js\\lib\\jsWindows\\jsWindows.js"],'public\\js\\lib\\jsWindows\\jsWindows.min.js');
+//fileZip.zipCss(["public\\js\\lib\\jsWindows\\jsWindows.css"],'public\\js\\lib\\jsWindows\\jsWindows.min.css');
 //导出类
 module.exports = fileZip;
