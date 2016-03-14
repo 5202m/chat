a23017b607fc339c508d40f3f58038398db70ded
dick <dick@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-03-14 05:53:49 +0000

    直播间手机版
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@2023 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- resources/config.js
+++ resources/config.js
@@ -3,20 +3,20 @@
  * Created by Alan.wu on 2015/4/18.
  */
 var config = {
-    clusterWorkCount:2,//开启多进程个数，如果该参数大于系统内核个数，默认是开启(cpu核数-1)个进程
+    clusterWorkCount:2,//开启多线程个数，如果该参数大于系统内核个数，默认是开启(cpu核数-1)个线程
     sessionConfig:{key:'connect.sid',secret:'pm@chat'},//session 对应key,secret
     redisUrlObj:{ host: '192.168.35.236', port: 6379 },	//链接redis缓存客户端连接
-    isAllowCopyHomeUrl:true,//是否允许copy链接（针对微信进入聊天室）
+    isAllowCopyHomeUrl:false,//是否允许copy链接（针对微信进入聊天室）
     pmApiUrl:'http://113.28.105.65:3000/api',//pmApi地址
-    goldApiUrl: 'http://192.168.35.160/goldoffice_api/RESTful',//黄金api地址
+    goldApiUrl: 'http://192.168.35.160/goldoffice_api/RESTful',//gwapi地址
     gwfxGTS2ApiUrl:'http://192.168.35.115:8080/Goldoffice_api/RESTful',//外汇GTS2 Api地址
     gwfxMT4ApiUrl:'http://192.168.75.40:8081/GwfxApi/RESTful',//外汇MT4 Api地址
     simulateApiUrl:'http://192.168.35.160/goldoffice_api_demo/RESTful',//模拟账户api地址
     socketServerUrl:{webSocket:'http://113.28.105.65:3002',socketIO:'http://113.28.105.65:3003'},
     liveUrl:"http://120.24.61.28:1935/test/123/playlist.m3u8",//手机直播地址
-    packetAcUrl:'http://testweb1.24k.hk/activity20160105/getActivityUrl',//红包活动连接
-    filesDomain: 'http://192.168.35.91:8090',//图片等文件访问域名
+    filesDomain: 'http://113.28.105.65:8090',//图片等文件访问域名
     web24kPath:'http://testweb1.24k.hk:8090',//24k信息地址
+    packetAcUrl:'http://testweb1.24k.hk/activity20160105/getActivityUrl',//红包活动连接
     mobile24kPath:'http://testweb1.24k.hk:8092',//24k信息地址
     //db
     dbURL:'mongodb://192.168.35.236/pm_mis',
