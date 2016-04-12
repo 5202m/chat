353a6393802564b03e5966448de00489148fe16d
alan <alan@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-04-12 01:53:17 +0000

    调整直播连接
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@2299 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- public/js/studio/admin.js
+++ public/js/studio/admin.js
@@ -25,9 +25,23 @@ var studioChat={
      */
     setVideo:function(){
         try{
+            $("#yyVideoDiv").html("");
             if($("#yyVideoDiv embed").length==0){
                 var aDom=$("#yyVideoDiv"),yc=aDom.attr("yc"),mc=aDom.attr("mc");
-                $('<embed src="'+'http://yy.com/s/'+yc+(common.isValid(mc)?'/'+mc:'')+'/yyscene.swf" quality="high" width="100%" height="100%" align="middle" allowScriptAccess="never" allowFullScreen="true" mode="transparent" type="application/x-shockwave-flash"></embed>').appendTo('#yyVideoDiv');
+                if('live'==yc) {
+                    var url='rtmp://ct.phgsa.cn/live/'+common.trim(mc);
+                    var sdHtml = '<div style="position: relative; width: 100%; height: 100%; left: 0px; top: 0px;">' +
+                        '<object type="application/x-shockwave-flash" id="sewise_player" name="sewise_player" data="/js/lib/flash/SewisePlayer.swf" width="100%" height="100%">' +
+                        '<param name="allowfullscreen" value="true">' +
+                        '<param name="wmode" value="transparent">' +
+                        '<param name="allowscriptaccess" value="always">' +
+                        '<param name="flashvars" value="autoStart=true&amp;programId=&amp;shiftTime=&amp;buffer=5&amp;lang=zh_CN&amp;type=rtmp&amp;serverApi=ServerApi.execute&amp;skin=/js/lib/flash/skins/liveOrange.swf&amp;title=&amp;draggable=true&amp;published=0&amp;streamUrl=' + url + '&amp;duration=3600&amp;poster=&amp;flagDatas=&amp;videosJsonUrl=&amp;adsJsonData=&amp;statistics=&amp;customDatas=&amp;playerName=Sewise Player&amp;clarityButton=enable&amp;timeDisplay=disable&amp;controlBarDisplay=enable&amp;topBarDisplay=disable&amp;customStrings=&amp;volume=0.6&amp;key=&amp;trackCallback=">' +
+                        '</object>' +
+                        '</div>';
+                    $("#yyVideoDiv").html(sdHtml);
+                }else{
+                    $('<embed src="'+'http://yy.com/s/'+yc+(common.isValid(mc)?'/'+mc:'')+'/yyscene.swf" quality="high" width="100%" height="100%" align="middle" allowScriptAccess="never" allowFullScreen="true" mode="transparent" type="application/x-shockwave-flash"></embed>').appendTo('#yyVideoDiv');
+                }
             }
             $("#yyVideoDiv").show();
         }catch(e){
