aef56816f6f37ecc4cb441d00b30b48a6d2abec9
jade <jade@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-08-17 03:01:10 +0000

    添加财经资讯
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@3972 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- template/hx/theme2/static/js/room.js
+++ template/hx/theme2/static/js/room.js
@@ -324,7 +324,7 @@ var roomJS={
      */
     setVideoList:function(){
         studioMbPop.loadingBlock($("#videosTab"));
-        this.getArticleList("teach_video_base,teach_video_gw,teach_video_expert",this.userInfo.groupId,0,1,100,'{"sequence":"desc","publishStartDate":"desc"}',null,function(dataList){
+        this.getArticleList("teach_video_base,teach_video_gw,teach_video_expert,teach_video_financial",this.userInfo.groupId,0,1,100,'{"sequence":"desc","publishStartDate":"desc"}',null,function(dataList){
             var loc_panel = $("#videosTab .boxcont");
             loc_panel.html("");
             if(dataList && dataList.result==0){
