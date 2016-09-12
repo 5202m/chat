a006cbefac87802aafc2d553936787423a7ca7ac
darren <darren@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-09-12 09:04:19 +0000

    增加晒单提交推送到公聊框
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@4299 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- routes/web/base.js
+++ routes/web/base.js
@@ -1136,10 +1136,10 @@ router.post('/addShowTrade', function(req, res){
         res.json({'isOK':false, 'msg':'参数错误'});
     }else{
         showTradeService.addShowTrade(params, function(result){
-            res.json(result);
             if(params.tradeType == 2){ //客户晒单
-               chatService.sendMsgToRoom(true,params.groupType,params.groupId,"notice",{type:chatService.noticeType.showTrade,data:params});
+                chatService.sendMsgToRoom(true,params.groupType,params.groupId,"notice",{type:chatService.noticeType.showTrade,data:params});
             }
+            res.json(result);
         });
     }
 });
