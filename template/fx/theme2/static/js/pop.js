7277f1b9067ef6b756ae11bec297667d12a5727f
dick <dick@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-09-21 08:17:05 +0000

    注册抽奖活动
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@4351 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- template/fx/theme2/static/js/pop.js
+++ template/fx/theme2/static/js/pop.js
@@ -592,7 +592,7 @@ var studioMbReg = {
                     return false;
                 }else{
                     studioMbPop.showMessage("注册成功");
-                    $("#reg1Form_lpBtn").attr("href", "http://testweb.gwfx.com/activity/studioLottery/index.html?userId=" + result.userId + "#ba");
+                    $("#reg1Form_lpBtn").attr("href", "http://www.gwfx.com/activity/studioLottery/index.html?userId=" + result.userId + "#ba");
                     studioMbPop.popShow($("#reg1Pop"));
                 }
             },true,function(){
