a00aaf313deedbc3b217fa191c9e6201d27e2b64
jade <jade@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-10-25 09:15:41 +0000

    修改合法昵称提示语
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@4651 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- template/pm/theme1/static/js/index_box.js
+++ template/pm/theme1/static/js/index_box.js
@@ -828,7 +828,7 @@ var box={
                 box.showTipBox('请输入昵称');
                 return;
             }else if(common.isRightName(nickName)){
-                box.showTipBox('昵称不合法');
+                box.showTipBox('昵称必须由中文、英文、不超过连续4个数字并且长度不超过8个字符');
                 return;
             } else {
                 $(this).prop('disabled', true);
