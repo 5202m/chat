223ed24f961fdb9ef1db8ba9ee28a1c73ecfede4
jade <jade@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-10-27 09:48:01 +0000

    修改注册时密码未加密问题
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@4725 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- routes/web/base.js
+++ routes/web/base.js
@@ -712,7 +712,7 @@ router.post('/reg',function(req, res){
                 //验证码通过校验
                 studioService.checkClientGroup(params.mobilePhone,null,common.getTempPlatformKey(userSession.groupType),function(clientGroup, accountNo){
                     var thirdId = userSession.thirdId || null;
-                    var userInfo={mobilePhone:params.mobilePhone, ip:common.getClientIp(req), groupType:userSession.groupType, accountNo: accountNo, thirdId:thirdId, pwd:params.password,item:params.item,userId:userSession.userId};
+                    var userInfo={mobilePhone:params.mobilePhone, ip:common.getClientIp(req), groupType:userSession.groupType, accountNo: accountNo, thirdId:thirdId, pwd:common.getMD5(constant.pwdKey+params.password),item:params.item,userId:userSession.userId};
                     studioService.studioRegister(userInfo,clientGroup,function(result){
                         if(result.isOK){
                             req.session.studioUserInfo={groupType:userSession.groupType,clientStoreId:userSession.clientStoreId,firstLogin:true,isLogin:true,mobilePhone:userInfo.mobilePhone,userId:userInfo.userId,defGroupId:userInfo.defGroupId,clientGroup:userInfo.clientGroup,nickname:userInfo.nickname};
