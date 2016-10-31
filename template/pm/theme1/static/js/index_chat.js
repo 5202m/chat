4569c8fce69a391fdc1aa5c738bccdc47a811c83
jade <jade@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-10-31 01:09:21 +0000

    修改联系老师按钮属性值
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@4791 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- template/pm/theme1/static/js/index_chat.js
+++ template/pm/theme1/static/js/index_chat.js
@@ -1292,7 +1292,7 @@ var chat={
                     $('.mult_dialog a[uid='+row.userId+']').attr('online', true);
                     onLineNum = onLineNum - 1;
                 }else if(row.userType == 2 && $('#analystbar a[uid="'+row.userId+'"]').length == 0){
-                    $('#analystbar').append('<a href="javascript:void(0);" t="1" avs="'+row.avatar+'" nk="'+row.nickname+'" class="contactbtn" uid="'+row.userId+'" utype="'+row.userType+'" onclick="_gaq.push([\'_trackEvent\', \'pmchat_studio\', \'right_lts_LianXiTeacher\', \'content_right\', 1, true]);">联系'+row.nickname+'</a>');
+                    $('#analystbar').append('<a href="javascript:void(0);" t="0" avs="'+row.avatar+'" nk="'+row.nickname+'" class="contactbtn" uid="'+row.userId+'" utype="'+row.userType+'" onclick="_gaq.push([\'_trackEvent\', \'pmchat_studio\', \'right_lts_LianXiTeacher\', \'content_right\', 1, true]);">联系'+row.nickname+'</a>');
                 }
             }
             chat.contactAnalystEvent();
@@ -1328,7 +1328,7 @@ var chat={
                     if(data.online){
                         var onLineNum = 0;
                         if(userInfoTmp.userType == 2 && $('#analystbar a[uid="'+userInfoTmp.userId+'"]').length == 0){
-                            $('#analystbar').append('<a href="javascript:void(0);" t="0" avs="'+userInfoTmp.avatar+'" nk="'+userInfoTmp.nickname+'" class="contactbtn" uid="'+userInfoTmp.userId+'" utype="'+userInfoTmp.userType+'">联系'+userInfoTmp.nickname+'</a>');
+                            $('#analystbar').append('<a href="javascript:void(0);" t="0" avs="'+userInfoTmp.avatar+'" nk="'+userInfoTmp.nickname+'" class="contactbtn" uid="'+userInfoTmp.userId+'" utype="'+userInfoTmp.userType+'" onclick="_gaq.push([\'_trackEvent\', \'pmchat_studio\', \'right_lts_LianXiTeacher\', \'content_right\', 1, true]);">联系'+userInfoTmp.nickname+'</a>');
                             onLineNum += 1;
                         }
                         chat.contactAnalystEvent();
