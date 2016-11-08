c92f8afe6c2f290830c176d5d701871bf2c015d8
dick <dick@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-11-08 06:09:52 +0000

    增加500错误日志
    git-svn-id: svn://172.30.3.30/pm/pm_chat/trunk@4890 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(function(req, res, next) {
 // error handlers
 // no stacktraces leaked to user（500请求错误处理）
 app.use(function(err, req, res, next) {
+    console.error("500错误", err);
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
